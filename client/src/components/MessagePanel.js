f6522ee4f4cfc189d531a69e5fb17aca44ea330e
imzhangxian <imzhangxian@gmail.com>
2021-06-06 01:19:37 +0800

    Added Icons; Click hide left drawer and message panel

--- client/src/components/MessagePanel.js
+++ client/src/components/MessagePanel.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function MessagePanel() {
-    const [show, setShow] = useState(false);
+    const [showMessages, setShowMessages] = useState(false);
     const [messages, setMessages] = useState([
         { severity: "severe", plant: "New Plant 01", stage: "Primary", text: "exceed max capacity!!" },
         { severity: "normal", plant: "New Plant 02", stage: "Secondary", text: "running normally." },
@@ -10,12 +10,33 @@ function MessagePanel() {
         { severity: "severe", plant: "Plant 05", stage: "Primary", text: "exceed max capacity!!" }
     ]);
 
+    const messagepopup = useRef();
+    const togglebutton = useRef();
+
+    useEffect(() => {
+      // add when mounted
+      document.addEventListener("mousedown", handleClick);
+      // return function to be called when unmounted
+      return () => {
+        document.removeEventListener("mousedown", handleClick);
+      };
+    }, []);
+
+    const handleClick = e => {
+      if (messagepopup.current.contains(e.target) || togglebutton.current.contains(e.target)) {
+        // inside click
+        return;
+      }
+      // outside click 
+      setShowMessages(false);
+    };
+
     return (
         <div>
-            <button className="btn btn-outline-primary message-panel-ctl" onClick={() => setShow(!show)}>+</button>
-            <div className={`overflow-auto message-panel ${show ? "active" : ""}`}>
+            <button ref={togglebutton} className="btn btn-outline-primary message-panel-ctl" onClick={() => setShowMessages(!showMessages)}>+</button>
+            <div ref={messagepopup} className={`message-panel ${showMessages ? "active" : ""}`}>
                 <h3>Messages</h3>
-                <div className="message-link">
+                <div className="overflow-auto message-list">
                     <ul>
                         {messages.map(message => {
                             let messageClass = "alert alert-success";
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
+import { HiGlobe, HiViewList, HiChartPie, HiServer, HiInformationCircle } from 'react-icons/hi'
 import './css/Navbar.css'
 
 function Navbar() {
@@ -8,30 +9,51 @@ function Navbar() {
         setSidebar(! sidebar);
     }
 
+    const leftdrawer = useRef();
+    const togglebutton = useRef();
+
+    useEffect(() => {
+      // add when mounted
+      document.addEventListener("mousedown", handleClick);
+      // return function to be called when unmounted
+      return () => {
+        document.removeEventListener("mousedown", handleClick);
+      };
+    }, []);
+
+    const handleClick = e => {
+      if (leftdrawer.current.contains(e.target) || togglebutton.current.contains(e.target)) {
+        // inside click
+        return;
+      }
+      // outside click 
+      setSidebar(false);
+    };
+
     return (
       <>
       <nav className="nav navbar-dark bg-dark nav-header">
-        <button className="navbar-toggler" type="button" onClick={showSidebar}>
+        <button ref={togglebutton} className="navbar-toggler" type="button" onClick={showSidebar}>
           <span className="navbar-toggler-icon"></span>
         </button>
         <span className="navbar-brand mb-0 h1">Smart Water Integral</span>
       </nav>
-      <div className={sidebar ? 'left-drawer active' : 'left-drawer'}>
+      <div ref={leftdrawer} className={sidebar ? 'left-drawer active' : 'left-drawer'}>
         <ul className="nav nav-pills nav-fill">
           <li className="nav-item">
-            <a className="nav-link" href="/">Home</a>
+            <a className="nav-link" href="/"><HiGlobe /> Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/plants">Plants</a>
+            <a className="nav-link" href="/plants"><HiViewList /> Plants</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/about">About</a>
+            <a className="nav-link" href="/about"><HiInformationCircle /> About</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/reports">Reports</a>
+            <a className="nav-link" href="/reports"><HiChartPie /> Reports</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/manage">Manage</a>
+            <a className="nav-link" href="/manage"><HiServer /> Manage</a>
           </li>
         </ul>
       </div>
